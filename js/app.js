38012f43eace2798c7e89de03aba185a601a6a78
James Kekong <jameskekong@gmail.com>
2018-05-16 15:27:29 +0100

    fixed indentation errors

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let open = [];
 let moves = 0;
 let matchedCard = document.getElementsByClassName("match");
 let matched = 8;
-let interval; 
+let interval;
 let star;
 let counter = document.querySelector(".moves");
 let minutes = document.querySelector(".minutes");
@@ -14,19 +14,20 @@ let seconds = document.querySelector(".seconds");
 let winContainer = document.querySelector(".win");
 
 // function for adding eventlistener if a card is clicked
-let clicked = function (){
-   this.classList.toggle("open");
-   this.classList.toggle("locked");
-   openAdd.call(this);
+let clicked = function() {
+    this.classList.toggle("open");
+    this.classList.toggle("locked");
+    openAdd.call(this);
 };
 
-for (let i = 0; i < cards.length; i++){
-   cards[i].addEventListener("click", clicked);
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", clicked);
 }
 
 //this function randomly shuffles the content of an array
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -40,137 +41,138 @@ function shuffle(array) {
 }
 
 //function to create new deck of shuffled cards
-function newDeck(){
-  let deck = document.querySelector(".deck");
-  const newDeck = document.createElement("ul");
-  newDeck.className = "deck";
-  let shuffledCards = shuffle(cards);
-  for (let i= 0; i < shuffledCards.length; i++){
-    newDeck.appendChild(shuffledCards[i]);
-    shuffledCards[i].classList.remove("open", "match", "locked");
-   }
-   container.removeChild(deck);
-   container.appendChild(newDeck);
-}  
+function newDeck() {
+    let deck = document.querySelector(".deck");
+    const newDeck = document.createElement("ul");
+    newDeck.className = "deck";
+    let shuffledCards = shuffle(cards);
+    for (let i = 0; i < shuffledCards.length; i++) {
+        newDeck.appendChild(shuffledCards[i]);
+        shuffledCards[i].classList.remove("open", "match", "locked");
+    }
+    container.removeChild(deck);
+    container.appendChild(newDeck);
+}
 
 
 //function for adding cards to open array and comparing them
-function openAdd(){
-  open.push(this);
-  if(open.length === 2){
-    movesCounter();
-    if (open[0].type === open[1].type){
-      open[0].classList.add("match","locked");
-      open[1].classList.add("match", "locked");
-      open[0].classList.remove("open");
-      open[1].classList.remove("open");
-      open = [];
-      matched -= 1;
-      if (!matched){
-        winner();
-      }
-    }
-    else{
-      /* this prevents clicks on all cards while two cards are compared*/
-    open[0].classList.add("shake");
-    open[1].classList.add("shake");
-    Array.prototype.filter.call(cards, function(card){
-      card.classList.add("locked");
-    });
-    /* this prevents user from clicking on matched cards,
-     and enable clicks on all remaining cards*/
-    setTimeout(function(){
-      open[0].classList.remove("open","shake");
-      open[1].classList.remove("open","shake");
-      Array.prototype.filter.call(cards, function(card){
-        card.classList.remove("locked");
-        for (var i = 0; i < matchedCard.length; i++){
-          matchedCard[i].classList.add("locked");
-        }
-      });
-      open = [];
-    },1100);
+function openAdd() {
+    open.push(this);
+    if (open.length === 2) {
+        movesCounter();
+        if (open[0].type === open[1].type) {
+            open[0].classList.add("match", "locked");
+            open[1].classList.add("match", "locked");
+            open[0].classList.remove("open");
+            open[1].classList.remove("open");
+            open = [];
+            matched -= 1;
+            if (!matched) {
+                winner();
+            }
+        } else {
+            /* this prevents clicks on all cards while two cards are compared*/
+            open[0].classList.add("shake");
+            open[1].classList.add("shake");
+            Array.prototype.filter.call(cards, function(card) {
+                card.classList.add("locked");
+            });
+            /* this prevents user from clicking on matched cards,
+             and enable clicks on all remaining cards*/
+            setTimeout(function() {
+                open[0].classList.remove("open", "shake");
+                open[1].classList.remove("open", "shake");
+                Array.prototype.filter.call(cards, function(card) {
+                    card.classList.remove("locked");
+                    for (var i = 0; i < matchedCard.length; i++) {
+                        matchedCard[i].classList.add("locked");
+                    }
+                });
+                open = [];
+            }, 1100);
 
-    }  
-  }
+        }
+    }
 }
 
 //function for creating moves and setting stars ratings
-function movesCounter(){
-  let counter = document.querySelector(".moves");
-  let step = 12;
-  moves += 1;
-  counter.textContent = moves;
-  if (moves % step === 0 && moves < step*3){
-    document.querySelector(".stars").children[0].remove();
-  }
-  if (moves < 12){
-    star = 3;
-  }else if ( moves == 12 || moves < 24){
-    star = 2;
+function movesCounter() {
+    let counter = document.querySelector(".moves");
+    let step = 12;
+    moves += 1;
+    counter.textContent = moves;
+    if (moves % step === 0 && moves < step * 3) {
+        document.querySelector(".stars").children[0].remove();
+    }
+    if (moves < 12) {
+        star = 3;
+    } else if (moves == 12 || moves < 24) {
+        star = 2;
 
-  }else{
-    star = 1;
-  } 
+    } else {
+        star = 1;
+    }
 
-  if (moves === 1){
-    startTimer();
-    interval = window.setInterval(startTimer, 1000);
-  }
+    if (moves === 1) {
+        startTimer();
+        interval = window.setInterval(startTimer, 1000);
+    }
 }
 
 //function to start the game timer
 var time = 0;
+
 function startTimer() {
-  time += 1;
-  if (time < 3600) {
-    minutes.textContent = Math.floor(time / 60) > 9 ? Math.floor(time / 60) : "0" + Math.floor(time / 60);
-    seconds.textContent = time % 60 > 9 ? time % 60 : "0" + time % 60;
-  } else {
-    reset();
-  }
+    time += 1;
+    if (time < 3600) {
+        minutes.textContent = Math.floor(time / 60) > 9 ? Math.floor(time / 60) : "0" + Math.floor(time / 60);
+        seconds.textContent = time % 60 > 9 ? time % 60 : "0" + time % 60;
+    } else {
+        reset();
+    }
 }
 
 // function to display congratultions modal when winning conditions are met
-function winner(){
-  if (matchedCard.length === 16){
-    clearInterval(interval);
-    let movesSpan = winContainer.querySelector(".moves");
-    let starsSpan = winContainer.querySelector(".stars");
-    let timeSpan = winContainer.querySelector(".time");
+function winner() {
+    if (matchedCard.length === 16) {
+        clearInterval(interval);
+        let movesSpan = winContainer.querySelector(".moves");
+        let starsSpan = winContainer.querySelector(".stars");
+        let timeSpan = winContainer.querySelector(".time");
 
-    movesSpan.textContent = moves;
-    starsSpan.textContent = star;
-    timeSpan.textContent = minutes.textContent +":"+ seconds.textContent;
+        movesSpan.textContent = moves;
+        starsSpan.textContent = star;
+        timeSpan.textContent = minutes.textContent + ":" + seconds.textContent;
 
-    container.classList.add("hidden");
-    winContainer.classList.remove("hidden");
-  }
-  replay();
+        container.classList.add("hidden");
+        winContainer.classList.remove("hidden");
+    }
+    replay();
 }
 
 //function to close congratulations modal and replay
-function replay(){
-  let playAgain = document.querySelector(".playAgain");
-  playAgain.addEventListener("click", function(){
-    container.classList.remove("hidden");
-    winContainer.classList.add("hidden");
-    reset();  
-  });
+function replay() {
+    let playAgain = document.querySelector(".playAgain");
+    playAgain.addEventListener("click", function() {
+        container.classList.remove("hidden");
+        winContainer.classList.add("hidden");
+        reset();
+    });
 }
 
 //function to reset all the game parameters to default
-document.body.onload = reset(); 
+document.body.onload = reset();
 restart[0].addEventListener("click", reset);
+
 function reset() {
-  clearInterval(interval);
-  open = [];
-  moves = 0;
-  counter.textContent = "0";
-  document.querySelector(".stars").innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
-  minutes.textContent = "00";
-  seconds.textContent = "00";
-  time = 0;
-  matched = 8;
-  newDeck();
+    clearInterval(interval);
+    open = [];
+    moves = 0;
+    counter.textContent = "0";
+    document.querySelector(".stars").innerHTML = `<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>`;
+    minutes.textContent = "00";
+    seconds.textContent = "00";
+    time = 0;
+    matched = 8;
+    newDeck();
 }
