d2516820f94dd4e19c4de68e140410aa8284e4b0
Kekong James <31131362+bigboy92@users.noreply.github.com>
2024-10-11 19:45:36 +0100

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,55 +1,54 @@
-
-import argparse
-import numpy as np
+import os
 import json
-import tensorflow as tf
-import tensorflow_hub as hub
+import time
+
+import numpy as np
+import argparse as arg
 from PIL import Image
 
+import tensorflow as tf
+import tensorflow_hub as hub
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--input', default='./test_images/cautleya_spicata.jpg', action="store", type = str, help='Image path')
-parser.add_argument('--my_model', default='my_model.h5', action="store", type = str, help='Classifier path')
-parser.add_argument('--top_k', default=5, action="store", type=int, help='The top K most likely classes')
-parser.add_argument('--category_name', default='./label_map.json', action="store", type=str, help='Path of JSON file maps labels to flower names')
 
-arg_parser = parser.parse_args()
-image_path = arg_parser.input
-myModel_path = arg_parser.my_model
-top_k = arg_parser.top_k
-category_name = arg_parser.category_name
 
+def load_model(path):
+    model_path = './' + path
+    load_model = tf.keras.models.load_model(path ,custom_objects={'KerasLayer':hub.KerasLayer}, compile=False)
+    return load_model
+                                                 
 def process_image(image):
     image_size = 224
     image = tf.cast(image, tf.float32)
     image = tf.image.resize(image, (image_size, image_size))
     image /= 255
-    return image.numpy()
+    image = image.numpy()
+    return image    
+    
+def predict(image_path, model, top_k=5):
+    im = Image.open(image_path)
+    test_image = np.asarray(im)
+    processed_test_image = process_image(test_image)
+    processed_test_image = np.expand_dims(processed_test_image, axis=0)
+    preds = model.predict(processed_test_image)
+    probs = - np.partition(-preds[0], top_k)[:top_k]
+    classes = np.argpartition(-preds[0], top_k)[:top_k]
+    return probs, classes
 
-def predict(image_path, model, top_k):
-    if top_k < 0:
-        print('Top_k must be greater then or equals 0')
-        exit()
+parser = arg.ArgumentParser()
 
-    image = Image.open(image_path)
-    image = np.asarray(image)
-    processed_image = process_image(image)
-    expanded_image = np.expand_dims(processed_image, axis=0)
-    predictions = model.predict(expanded_image)
-    probs, labels = tf.nn.top_k(predictions, k=top_k)
-    probs = list(probs.numpy()[0])
-    labels = list(labels.numpy()[0])
-    return probs, labels
+parser.add_argument('img_path', type=str)
+parser.add_argument('model_path', type=str)
+parser.add_argument('--top_k', type=int, default=5)
+parser.add_argument('--category_names', type=str, required=True)
 
-if __name__== "__main__":
-    with open(category_name, 'r') as f:
-        class_name = json.load(f)
-    reloaded_keras_model = tf.keras.models.load_model(myModel_path, custom_objects = {'KerasLayer':hub.KerasLayer})
-    probs, labels = predict(image_path, reloaded_keras_model, top_k)
-    print ("\n  Top {} Classes  \n".format(top_k))
-    for i, prob, label in zip(range(1, top_k+1), probs, labels):
-        print('number: ', i)
-        print('Label is:', label)
-        print('Class name is:  ', class_name[str(label+1)].title())
-        print('Probability is :', prob)
-        
+args = parser.parse_args()
+model = load_model(args.model_path)
+
+prob, classes = predict(image_path=args.img_path, model=model, top_k=args.top_k)
+
+with open(args.category_names, 'r') as f:
+    class_names = json.load(f)
+    classes = {int(i): class_names[str(i)] for i in classes}
+    
+print('Predictions:', classes)
+print('Probability:', prob)
